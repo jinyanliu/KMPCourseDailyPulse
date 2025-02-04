14310414ffc32c69ec69f048a25633c436208a36
JinYan Liu <jane.jinyan.liu@gmail.com>
2025-02-04 23:07:07 +0100

    Format date

--- shared/src/commonMain/kotlin/se/sugarest/dailypulse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/se/sugarest/dailypulse/articles/ArticlesUseCase.kt
@@ -1,5 +1,13 @@
 package se.sugarest.dailypulse.articles
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+
 class ArticlesUseCase(
     private val service: ArticlesService
 ) {
@@ -13,8 +21,24 @@ class ArticlesUseCase(
             Article(
                 title = articleRaw.title,
                 desc = articleRaw.desc ?: "Click to find out more",
-                date = articleRaw.date,
-                imageUrl = articleRaw.imageUrl ?: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg"
+                date = getDaysAgoString(articleRaw.date),
+                imageUrl = articleRaw.imageUrl
+                    ?: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg"
             )
         }
+
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
+    }
 }
